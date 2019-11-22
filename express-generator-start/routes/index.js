4fd29df47d4181b255a9c42c4b1b5808982318f4
Ivan <ifrancesalcantara@gmail.com>
2019-11-22 19:40:46 +0100

    map centered to user when cookie and hits home page (/)

--- express-generator-start/routes/index.js
+++ express-generator-start/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const User = require('./../models/User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,8 +11,13 @@ router.get('/', function(req, res, next) {
   if(req.session.currentUser){
     console.log("cookie dected");
     
-    
-    res.render("secure/map");
+    User.findOne({_id: req.session.currentUser._id}) //CHANGE WHEN TIME TO MATCHING
+    .then( (user)=> {
+
+        const userHomeLocation = user.defaultLocation;
+        console.log("coords for /map: ", userHomeLocation)
+        res.render('secure/map', userHomeLocation)
+        })
     return;
   }
   console.log("no cookie");
