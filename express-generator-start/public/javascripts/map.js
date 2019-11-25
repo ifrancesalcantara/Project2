8fd7fcc337f41e3b5fdf8a917625991961419596
Ivan <ifrancesalcantara@gmail.com>
2019-11-25 20:10:19 +0100

    pre-pull

--- express-generator-start/public/javascripts/map.js
+++ express-generator-start/public/javascripts/map.js
@@ -1,4 +1,3 @@
-console.log("hi");
 const addCommentForm = document.querySelector(".add-comment-form")
 
 
@@ -7,11 +6,27 @@ const imagesDiv = document.createElement("div");
 const socialComment = document.createElement("img");
 const sightComment = document.createElement("img");
 const claimComment = document.createElement("img");
+
 imagesDiv.classList.add("add-comment-form-img-div");
 socialComment.classList.add("add-comment-form-img-social");
 sightComment.classList.add("add-comment-form-img-sight");
 claimComment.classList.add("add-comment-form-img-claim");
+
+socialComment.setAttribute("src", "./images/social.jpg");
+sightComment.setAttribute("src", "./images/sight.png");
+// claimComment.addAttribute("src", );
+
 imagesDiv.appendChild(socialComment)
 imagesDiv.appendChild(sightComment)
 imagesDiv.appendChild(claimComment)
-addCommentForm.appendChild(imagesDiv)
+addCommentForm.appendChild(imagesDiv);
+
+socialComment.addEventListener("click", ()=>{
+    console.log("social");
+    
+})
+
+sightComment.addEventListener("click", ()=>{
+    console.log("social");
+    
+})
