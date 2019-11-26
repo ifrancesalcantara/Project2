6f9abc21cf7277625692434f4bb4ef107d93a9b9
Boris Vogt <boris.vogt@posteo.de>
2019-11-26 21:23:01 +0100

    add style stylesheets/style.scss, views/layout.hbs, secure/map.hbs

---
+++ public/javascripts/layout.js
@@ -0,0 +1,15 @@
+// document.getElementById('comment-popup-button').addEventListener('click', () => {
+
+//     const commentMenu = document.getElementById('comment-popup');
+//     console.log('////>/>>////>/ STYLE before if' , commentMenu.style.display);
+    
+//         if ( commentMenu.style.display === 'none') {
+//             console.log('////>/>>////>/ STYLE after if' , commentMenu.style.display);
+//          return commentMenu.style.display === 'block';
+//         }
+//         else {
+//             return commentMenu.style.display === 'none';
+//         }
+        
+
+// });
